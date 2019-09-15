60fa3dac78235ae1b82868dbd818f9f8df79ab51
itachi-P <itachip38@gmail.com>
2019-09-15 23:44:50 +0900

    Change files

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <title>home</title>
+        <link rel="icon" href="{{ asset('fuca_tehepero_icon_32x32.ico') }}" sizes="32x32">
+    </head>
+    <body>
+        <p>ナッラマナム</p>
+        <img src="images/SpiceCurry_Nrmnm.jpg" />
+
+    </body>
+</html>
