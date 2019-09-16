26f32be3ef187bb711c174dcf899f4d08d594d4d
itachi-P <itachip38@gmail.com>
2019-09-16 23:48:37 +0900

    :racehorse: Add Uploading images Model & Controller.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,45 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Model\Image;
 
 class HomeController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
-        return view('home', $request);
+        $images = Image::all();
+        return view('home', ['images' => $images]);
+    }
+
+    /**
+     * ファイルアップロード処理
+     */
+    public function upload(Request $request)
+    {
+        $this->validate($request, [
+            'file' => [
+                'required', // 入力必須であること    
+                'file',     // アップロードされたファイルであること
+                'image',    // 画像ファイルであること
+                'mimes:jpeg,png',   // MIMEタイプを指定
+            ]
+        ]);
+
+        if ($request->file('file')->isValid([])) {
+            //$path = $request->file->store('public');
+            $filename = $request->file->getClientOriginalName(); //一意なID発行の方が望ましい
+            $move = $request->file->move('./images', $filename);
+
+            $images = new Image;
+            $images->fill(['filename' => $filename])->save();
+            $images = Image::all();
+            $parameters = ['filename' => $filename, 'images' => $images];
+            return view('home', $parameters);
+        } else {
+            return redirect()
+                ->back()
+                ->withInput()
+                ->withErrors();
+        }
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,54 @@
-<!doctype html>
+<!DOCTYPE HTML>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <title>home</title>
-        <link rel="icon" href="{{ asset('fuca_tehepero_icon_32x32.ico') }}" sizes="32x32">
-    </head>
-    <body>
-        <ol>
-            <li>
-                <p>ナッラマナム</p>
-                <img src="images/SpiceCurry/Nrmnm20190909.jpg" width="250px" height="200px" />
-            </li>
-            <li>
-                <p>iloilo</p>
-                <img src="images/SpiceCurry/iloilo20190524.jpg" width="250px" height="200px" />
-            </li>
-        </ol>
-    </body>
+<head>
+    <meta charset="utf-8">
+    <title>home</title>
+    <link rel="icon" href="{{ asset('fuca_tehepero_icon_32x32.ico') }}" sizes="32x32">
+</head>
+<body>
+    <ol>
+        <li>
+            <p>ナッラマナム</p>
+            <img src="images/SpiceCurry/Nrmnm20190909.jpg" width="250px" height="200px" />
+        </li>
+        <li>
+            <p>iloilo</p>
+            <img src="images/SpiceCurry/iloilo20190524.jpg" width="250px" height="200px" />
+        </li>
+    </ol>
+
+{{-- imagesテーブルから取得した保存済み画像リンク一覧 --}}
+@if (isset($images))    
+<ul>
+    @foreach ($images as $image)
+    <li>
+        <a href="images/{{ $image->filename }}" target="_brank">
+            <img src="images/{{ $image->filename }}" width="120px" height="100px" />
+        </a>   
+    </li>
+    @endforeach
+</ul>
+@endif
+
+<!-- エラーメッセージ。なければ表示しない -->
+@if ($errors->any())
+<ul>
+    @foreach ($errors as $error)
+    <li>{{ $error }}</li>
+    @endforeach
+</ul>
+@endif
+
+<!-- フォーム -->
+<form action="{{ url('upload') }}" method="POST" enctype="multipart/form-data">
+
+    <label for="photo">画像ファイル:</label>
+    <input type="file" class="form-control" name="file">
+    <br>
+    <hr>
+    {{ csrf_field() }}
+    <button class="btn btn-succes"> Upload </button>
+</form>
+
+</body>
 </html>
